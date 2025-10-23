f1ef317365c0f8e1b8ed07d189a5cb0dff0a731f
Olena <alenguhm@gmail.com>
2025-10-23 10:40:39 +0300

    add tasks

---
+++ src/task-6.ts
@@ -0,0 +1,8 @@
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+console.log(getFirstElement<number>([1, 2, 3])); // 1
+console.log(getFirstElement<string>(["a", "b", "c"])); // )"a"
+console.log(getFirstElement<boolean>([true, false, true])); // true
+console.log(getFirstElement(["a", 1, true]));
